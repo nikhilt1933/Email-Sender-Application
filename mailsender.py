40f19540cf53788eea9ceeb77f0b63d5666b4b32
Nikhil Thadeshwar <NIKHILTHADESHWAR2@GMAIL.COM>
2019-12-06 23:21:16 +0530

    Add files via upload

---
+++ mailsender.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 22:41:08 2019
+
+@author: Nikhil
+"""
+
+import smtplib
+
+# Send mail function
+def send_mail(gmail, to, password, message):
+    
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(gmail,password)
+    s.sendmail(gmail,to,message)
+    
+    s.quit()
+    
+#send_mail('soni7.nikhil@gmail.com','nikhilthadeshwar2@gmail.com','aksharnikhil','Swaminarayan')
